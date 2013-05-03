2ce45243d8b092c328775e92017d9421f78c30b2
Clay Miller <clay@smockle.com>
2013-05-03 12:18:13 -0500

    Added "oitapps", "oitappsdev", "oitappstest" and "oitappsmigr" shortlinks. Version bump.

--- auburn-redirect-tool/art.js
+++ auburn-redirect-tool/art.js
@@ -12,12 +12,16 @@ chrome.webRequest.onBeforeRequest.addListener(
 	if (~info.url.indexOf("http://fp.auburn.edu")) redirect = "https://fp.auburn.edu" + info.url.substring(info.url.indexOf("http://fp.auburn.edu") + "http://fp.auburn.edu".length);
 	if(~info.url.indexOf("fp/")) redirect = "https://fp.auburn.edu/" + info.url.substring(info.url.indexOf("fp/") + "fp/".length);
 	else if(~info.url.indexOf("cws/")) redirect = "http://cws.auburn.edu/" + info.url.substring(info.url.indexOf("cws/") + "cws/".length);
+	else if(~info.url.indexOf("oitapps/")) redirect = "http://oitapps.auburn.edu/" + info.url.substring(info.url.indexOf("oitapps/") + "oitapps/".length);
 	else if(~info.url.indexOf("dev/")) redirect = "http://cwsdev.auburn.edu/" + info.url.substring(info.url.indexOf("dev/") + "dev/".length);
 	else if(~info.url.indexOf("cwsdev/")) redirect = "http://cwsdev.auburn.edu/" + info.url.substring(info.url.indexOf("cwsdev/") + "cwsdev/".length);
+	else if(~info.url.indexOf("oitappsdev/")) redirect = "http://oitappsdev.auburn.edu/" + info.url.substring(info.url.indexOf("oitappsdev/") + "oitappsdev/".length);
 	else if(~info.url.indexOf("test/")) redirect = "http://cwstest.auburn.edu/" + info.url.substring(info.url.indexOf("test/") + "test/".length);
 	else if(~info.url.indexOf("cwstest/")) redirect = "http://cwstest.auburn.edu/" + info.url.substring(info.url.indexOf("cwstest/") + "cwstest/".length);
+	else if(~info.url.indexOf("oitappstest/")) redirect = "http://oitappstest.auburn.edu/" + info.url.substring(info.url.indexOf("oitappstest/") + "oitappstest/".length);
 	else if(~info.url.indexOf("migr/")) redirect = "http://cwsmigr.auburn.edu/" + info.url.substring(info.url.indexOf("migr/") + "migr/".length);
 	else if(~info.url.indexOf("cwsmigr/")) redirect = "http://cwsmigr.auburn.edu/" + info.url.substring(info.url.indexOf("cwsmigr/") + "cwsmigr/".length);
+	else if(~info.url.indexOf("oitappsmigr/")) redirect = "http://oitappsmigr.auburn.edu/" + info.url.substring(info.url.indexOf("oitappsmigr/") + "oitappsmigr/".length);
 	
 	/* We should make an ajax request to see if the targeted location actually exists. */
 	
@@ -28,12 +32,16 @@ chrome.webRequest.onBeforeRequest.addListener(
 	  "http://fp.auburn.edu/*",
       "*://fp/*",
 	  "*://cws/*",
+	  "*://oitapps/*",
 	  "*://dev/*",
 	  "*://cwsdev/*",
+	  "*://oitappsdev/*",
 	  "*://test/*",
 	  "*://cwstest/*",
+	  "*://oitappstest/*",
 	  "*://migr/*",
-	  "*://cwsmigr/*"
+	  "*://cwsmigr/*",
+	  "*://oitappsmigr/*"
     ]
   },
   ["blocking"]);
